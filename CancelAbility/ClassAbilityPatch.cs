a52129a6720031ffffe2e19945eeeef7375b7115
18107 <18107@users.noreply.github.com>
2024-06-08 13:36:42 +0930

    Add files via upload

---
+++ CancelAbility/ClassAbilityPatch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+
+namespace CancelAbility
+{
+    [HarmonyPatch(typeof(ClassAbility))]
+    internal class ClassAbilityPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("ActivationKeyPressed")]
+        static void ActivationKeyPressed(ClassAbility __instance)
+        {
+            if (__instance is FiniteDurationClassAbility ability && ability is not ChannelGrapplingHook)
+            {
+                if (ability.IsOngoing())
+                {
+                    float refund = (1 - ability.CurrentActiveDuration / ability.FiniteTimeDuration.Value) * ability.CooldownSeconds.Value;
+                    ability.StopAbility();
+                    ability.RefundCooldown(refund);
+                }
+                else
+                {
+                    float cooldown = ability.CurrentCooldown;
+                    ability.CurrentCooldown = 0;
+                    ability.StartAbility();
+                    ability.CurrentActiveDuration = (1 - cooldown / ability.CooldownSeconds.Value) * ability.FiniteTimeDuration.Value;
+                }
+            }
+        }
+    }
+}
---
+++ CancelAbility/VoidManagerPlugin.cs
@@ -0,0 +1,13 @@
+﻿using VoidManager.MPModChecks;
+
+namespace CancelAbility
+{
+    public class VoidManagerPlugin : VoidManager.VoidPlugin
+    {
+        public override MultiplayerType MPType => MultiplayerType.Client;
+
+        public override string Author => "18107";
+
+        public override string Description => "Allows abilities to be cancelled early for faster recharge";
+    }
+}
